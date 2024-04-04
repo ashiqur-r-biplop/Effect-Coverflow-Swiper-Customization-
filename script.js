1da3f4aead5e414be476868006b0f309cede759a
ashiqur-r-biplop <biplops22@gmail.com>
2024-04-05 03:07:38 +0600

    work complete

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
-var swiper = new Swiper(".mySwiper", {
+var swiper = new Swiper(".swiper-container", {
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
   effect: "coverflow",
   grabCursor: true,
   centeredSlides: true,
   slidesPerView: "auto",
   coverflowEffect: {
-    rotate: 60,
+    rotate: 50,
     stretch: 0,
-    depth: 100,
+    depth: 200,
     modifier: 1,
-    slideShadows: true,
+    slideShadows: false,
   },
-  pagination: {
-    el: ".swiper-pagination",
-  },
+  loop: true,
 });
